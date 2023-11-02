7655ba27e5d05c7f1f3dbb4d8acaedba2d812703
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-02 10:14:03 +0000

    added total price variable

--- index.php
+++ index.php
@@ -27,13 +27,22 @@
     $online = false;
     $for_sale = true;
 
+    $total = null; //no value
+
     echo "Hello {$name} <br>"; 
     echo "You like {$food}<br>";
     echo "Your email is {$email}<br>";
     echo "You are {$age} years old<br>";
     echo "I would buy {$quantity} items<br>";
     echo "Your pizza cost \${$price}<br>";// \$ this is required if you want to put sign before the price
-    echo "Online status {$online}<br>" // dosn't show $online because it is false if $online was true then it will show 1 as a number 
+    echo "Online status {$online}<br>"; // dosn't show $online because it is false if $online was true then it will show 1 as a number 
+    echo "you have ordered {$quantity} x {$food}s<br>";
+
+    $total = $quantity * $price;
+    echo "your total is {$total}£";
+
+
+
 
 
 ?>
