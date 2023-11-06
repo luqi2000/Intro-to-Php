56387144a295ea6bc9572849ea79eb9cf7bde091
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-06 12:27:16 +0000

    exercise if you are able to enter the site or not

---
+++ ifStatement.php
@@ -0,0 +1,27 @@
+<?php
+    //if statement = if some condition is true, do something 
+    //if condition is false, don't do it
+    // == is used for comparetion
+    // = is used for assignment something
+
+    $age = 101;
+
+    if($age >=100){ // the reason why i moved this statement first is because if this is true then the rest of code will not execute
+        echo "You are too old to enter this site";
+    }
+    else if ($age>=18){
+        echo "You may enter this site";
+    }
+    else if($age <= 0){
+        echo "That wasn't a valid age";
+
+    }
+    else{
+        echo "You must 18+ to be enter";
+    }
+
+
+    //------------------------------------------------------------------------
+
+    
+?>
