612e70d6084613e64d98434b20a7ffa8ceaec998
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-07 09:46:58 +0000

    temperature is good or bad?

--- logicalOperator.php
+++ logicalOperator.php
@@ -1,8 +1,23 @@
 <?php
-    // Logical operators
+    // Logical operators = combine conditional statements
     // if(condition && condition2)
 
     //  && = true if both condition are true
-    //  !! = true if at least one condition is true 
+    //  || = true if at least one condition is true 
     //  ! = true if false. false if true 
+
+    $temp = 15;
+
+    if($temp >= 0 && $temp <=30){
+        echo "the weather is good<br>";
+    }else {
+        echo "the weather is bad<br>";
+    }
+//-------------------------------------------
+    if($temp < 0 || $temp >=30){
+        echo "the weather is bad";
+    }else {
+        echo "the weather is good";
+    }
+
 ?>
