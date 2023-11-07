5cd6e59b25b601a54a5f6262128a1751127724d8
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-11-07 09:33:29 +0000

    logical operators

---
+++ logicalOperator.php
@@ -0,0 +1,8 @@
+<?php
+    // Logical operators
+    // if(condition && condition2)
+
+    //  && = true if both condition are true
+    //  !! = true if at least one condition is true 
+    //  ! = true if false. false if true 
+?>
