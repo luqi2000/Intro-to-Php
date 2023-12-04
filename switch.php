e5fe3e4f538cfb50463b0a4158ee04f9d6eb197f
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-12-04 17:05:57 +0000

    switch example

--- switch.php
+++ switch.php
@@ -1,4 +1,29 @@
 <?php
 //switch = replacement to using many else if statements more efficient, less code to write
 
+$grade = "F";
+
+switch($grade){
+    case "A": 
+        echo "You did great!";
+        break; //You need to go out if it is ok 
+
+    case "B": 
+        echo "You did good!";
+        break;  
+
+    case "C": 
+        echo "You did Ok!";
+        break; 
+
+    case "D": 
+        echo "You did poorly!";
+        break;
+
+    case "E": 
+        echo "You failed!";
+        break;      
+    default:
+        echo "{$grade} is not valid";                
+}
 ?>
