e6325a5f0408ca42a7bc754da7a1fa16a976ebe8
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-12-04 22:08:59 +0000

    associative arrays

---
+++ associatedArray.php
@@ -0,0 +1,11 @@
+<?php
+//associative array = an array made of key => value pairs
+
+    $capitals = array ( "Usa" => "Washington",
+                        "Italy" => "Rome",
+                        "Pakistan"=> "Islamabad"
+                    );
+
+    echo $capitals["Italy"];                
+
+?>
