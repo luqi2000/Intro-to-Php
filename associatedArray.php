fd65e0e6fa8bdc62d38064fa7e9c8197740f2e95
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-12-07 12:54:46 +0000

    display only values

--- associatedArray.php
+++ associatedArray.php
@@ -11,16 +11,23 @@
     //change value of Usa from Washington to New york
     $capitals["Usa"] = "New York"; //changing value from key 
     $capitals["Uk"] = "London"; //adding key-value
-
-    $keys = array_keys($capitals); //this will return a new array
  
     //print all with key and value
     foreach($capitals as $key => $value){
         echo "{$key} = {$value}<br>";
     }
-
+    
     //display all keys
+    $keys = array_keys($capitals); //this will return a new array
+    
     foreach($keys as $key){
         echo "{$key}<br>";
+    }
+
+    //display all values
+    $values = array_values($capitals);
+
+    foreach($values as $value){
+        echo "{$value}<br>";
     }
 ?>
