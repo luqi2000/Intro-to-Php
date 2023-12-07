deab0962d8e97b09b46490bcf7d5b3e89fea7935
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-12-07 12:22:41 +0000

    changing value and adding value into array

--- associatedArray.php
+++ associatedArray.php
@@ -9,7 +9,8 @@
     echo $capitals["Italy"] . "<br>";// will print Rome
 
     //change value of Usa from Washington to New york
-    $capitals["Usa"] = "New York";
+    $capitals["Usa"] = "New York"; //changing value from key 
+    $capitals["Uk"] = "London"; //adding key-value
 
     //print all with key and value
     foreach($capitals as $key => $value){
