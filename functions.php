a56df70af676f4a65dc4fc8038cddc9eb38e35ab
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-12-18 12:30:42 +0000

    happy birthday!

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+    function happy_birthday($first_name, $age){
+        echo "Happy birthday dear {$first_name}!<br>";
+        echo "Happy birthday to you!<br>";
+        echo "Happy birthday dear you!<br>";
+        echo "You are {$age} years old!<br><br>";
+    }
+
+    happy_birthday("Spongebob", 30);
+
+    happy_birthday("Luqman", 24);
+?>
