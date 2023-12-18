b5e7f4e9abe7714691f71c93246a8cc2829da94a
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-12-18 10:52:08 +0000

    isset() and empty()

--- issetEmpty.php
+++ issetEmpty.php
@@ -1,4 +1,8 @@
 <?php
-    //isset() = returns True if a variable is declared and not null
-    //empty() = returns True if a variable is not declared, false, null, ""
+    //isset() = returns True if a variable is declared and not null  ----> 1 means true
+    //empty() = returns True if a variable is not declared, false, null, "" -----> not diplay anything means false
+
+    $username = "LuqmanCode";
+
+    echo isset($username);
 ?>
