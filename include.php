3f60e87f21b9c44827660889e21932f14d753525
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2024-01-02 14:44:34 +0000

    example of include function

---
+++ include.php
@@ -0,0 +1,6 @@
+<?php
+    //include() = copies the content of a file (php/html/text)
+    //and include it in your php file
+    //Sections of our website became reusable, changes only need to be made in one place
+    include("index.php");
+?>
