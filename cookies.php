8fdab73a27c8bb7bfa741191d189bd30ee48b788
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2024-01-02 18:45:03 +0000

    prints key and value of each cookie

---
+++ cookies.php
@@ -0,0 +1,18 @@
+<?php
+    //cookie = information about a user stored in a user's web-browser targeted  
+    //(key,value, time, Path )
+    //86400 = 1 days
+    //On google go to inspect then click on 
+
+
+    setcookie("fav_food", "pizza",time() + (86400*2), "/");
+    setcookie("fav_drink", "cofee",time() + (86400*3), "/");
+
+    setcookie("fav_dessert", "caramello",time() - 0 , "/"); // with - 0 cookies is not showing 
+
+    foreach($_COOKIE as $key => $value){
+        echo "{$key} = {$value}<br>";
+
+    }
+
+?>
