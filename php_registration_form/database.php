3f9213c033adb69e95a84ea32787880b4d90bb43
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2024-01-03 21:10:41 +0000

    added database

---
+++ php_registration_form/database.php
@@ -0,0 +1,25 @@
+<!--  
+1) open xammp manager-osx and make sure mysql and apache are running
+2) On google go to : http://localhost/phpmyadmin/ 
+3) click to database, write name of database examble: businessdb and then click on create
+4) close 
+
+-->
+
+<?php
+    
+    $db_server = "localhost"; //name of the server
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "businessdb";
+    $conn = "";
+    try{
+        
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+        echo "Connected to mysql<br>";
+
+    }catch(mysqli_sql_exception){
+        echo "Could not connected!!";
+    }
+  
+?>
